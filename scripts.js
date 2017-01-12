4f3b899462212d9cc57a0a4102cb44b685ccca8f
Lisa Shiphrah <lisa.shiphrah@gmail.com>
2017-01-12 14:43:03 +0000

    Initial screen with animations

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
-	
-	
+	$('.sides').mouseenter(function(){
+		$(this).addClass('bounce');
+	});
+
+	$('.sides').mouseleave(function(){
+		$(this).removeClass('bounce');
+	});
+
 });
+
+function selectedSide(){
+
+}
