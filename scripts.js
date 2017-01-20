70236c4743a0e23c0fbb4a176269af9c6d416842
Lisa Shiphrah <lisa.shiphrah@gmail.com>
2017-01-20 11:24:11 +0000

    JS changes

--- scripts.js
+++ scripts.js
@@ -15,4 +15,20 @@ function selectedSide(value){
 	side = value;
 	$('#initGameContainer').hide();
 	$('#boardContainer').show();
+
+	initGame();
+}
+
+//Function responsible for starting the game
+function initGame() {
+	if(side === 'X') { //X starts playing
+
+	}
+	else {
+
+	}
+}
+
+function makeMovement(element){
+	alert($(element));
 }
